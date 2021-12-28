d44954604694faad33c3c1c25a7111d70dd752f4
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2021-12-27 22:43:18 -0300

    Exercicios VisualG

--- module/Portal/src/Model/Validacao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Portal\Model;
-
-abstract class Validacao
-{
-
-}
--- public/js/projetogeral/RegistroUsuario.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-    $("#btnNovoUsuario").click(function () {
-        alert('ok');
-    });
-
-});
